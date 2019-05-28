d4bcea6e2599aedbf55545d1dd1a54fcb0344b25
Rahmat Wahyu Hadi <rahmat.hadi@tiket.com>
2019-05-29 01:53:36 +0700

    Initial Commit

---
+++ gql.py
@@ -0,0 +1,54 @@
+import requests
+import json 
+import argparse
+
+
+
+# for api graphql instropection 
+def IntrospectionQuery(endpoint, filename):
+
+    url = endpoint
+    #  for headers request
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0", 
+        "Accept": "*/*", 
+        "Accept-Language": "id,en-US;q=0.7,en;q=0.3", 
+        "Accept-Encoding": "gzip, deflate", 
+        "content-type": "application/json"
+    }
+
+    # Post Request Query for grab schema IntrospectionQuery
+    body ={"query": "query IntrospectionQuery {__schema {queryType { name },mutationType { name },subscriptionType { name },types {...FullType},directives {name,description,args {...InputValue},onOperation,onFragment,onField}}}\nfragment FullType on __Type {kind,name,description,fields(includeDeprecated: true) {name,description,args {...InputValue},type {...TypeRef},isDeprecated,deprecationReason},inputFields {...InputValue},interfaces {...TypeRef},enumValues(includeDeprecated: true) {name,description,isDeprecated,deprecationReason},possibleTypes {...TypeRef}}\nfragment InputValue on __InputValue {name,description,type { ...TypeRef },defaultValue}\nfragment TypeRef on __Type {kind,name,ofType {kind,name,ofType {kind,name,ofType {kind,name}}}}"}
+    data = requests.post(url, headers=headers, json=body)
+    status =  data.status_code
+    
+    print ("\n[+] Checking endpoint ", endpoint ," .. ")
+    if (status == 400) :
+        print ("[+] IntrospectionQuery Now Allowed")
+
+    if (status == 200) :
+        print ("[+] IntrospectionQuery Allowed ")
+        print ("[+] Saving  IntrospectionQuery to folder output .. ")
+
+        save = (json.loads(data.text))
+        filename_saved = 'output/'+filename+".json"
+        with open (filename_saved, 'w') as outfile:
+            json.dump(save, outfile)
+        print ("[+] __schema saved in ", filename_saved)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", help="filename")
+    parser.add_argument("-u", help="URL for graphql endpoint")
+
+    argv = (parser.parse_args())
+    url  = argv.u
+    filename = argv.f
+
+    if (filename != None) or (url != None):
+        IntrospectionQuery(url, filename)
+    else :
+        print ("[!] Opps something wrong!")
+
+if __name__ == "__main__":
+    main()
